0ab598ab959804a4fa689c797e1bda7a9f3e43a4
jithinletz <154591940+jithinletz@users.noreply.github.com>
2025-08-20 17:04:59 +0530

    item description setting

--- CustomerAppApi/BETaskCustomerAPI/DAL/ItemDAL.cs
+++ CustomerAppApi/BETaskCustomerAPI/DAL/ItemDAL.cs
@@ -36,11 +36,12 @@ namespace BETaskAPI.DAL
                             while (reader.Read())
                             {
                                 Item itm = new Item();
-                                itm.ItemId = Convert.ToInt32(reader["item_id"]);
-                                itm.ItemName = Convert.ToString(reader["item_Name"]);
-                                itm.Price = Convert.ToInt32(reader["price"]);
-                                itm.Currency = Convert.ToString(reader["currency"]);
-                                itm.ImagePath = Convert.ToString(reader["image_path"]);
+                                itm.ItemId = reader.IsDBNull(reader.GetOrdinal("item_id")) ? 0 : Convert.ToInt32(reader["item_id"]);
+                                itm.ItemName = reader.IsDBNull(reader.GetOrdinal("item_Name")) ? string.Empty : Convert.ToString(reader["item_Name"]);
+                                itm.Price = reader.IsDBNull(reader.GetOrdinal("price")) ? 0 : Convert.ToInt32(reader["price"]);
+                                itm.Currency = reader.IsDBNull(reader.GetOrdinal("currency")) ? string.Empty : Convert.ToString(reader["currency"]);
+                                itm.ImagePath = reader.IsDBNull(reader.GetOrdinal("image_path")) ? string.Empty : Convert.ToString(reader["image_path"]);
+                                itm.Description = reader.IsDBNull(reader.GetOrdinal("description")) ? string.Empty : Convert.ToString(reader["description"]);
                                 lstItemDetails.Add(itm);
                             }
                         }
